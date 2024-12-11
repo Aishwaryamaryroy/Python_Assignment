f1c2918a917b2a621f5175cf8fbe5f4d1277b2ed
Aishwaryamaryroy <annammasroy@gmail.com>
2024-12-11 22:04:04 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,66 @@
+# Exercise 1
+print("Aishwarya Mary Roy")
+print("D42")
+print("aishwaryamaryroy@gmail.com")
+
+# Exercise 2
+print("Aishwarya Mary Roy\nD42\naishwaryamaryroy@gmail.com")
+
+# Exercise 3
+a = 14
+b = 7
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+
+# Exercise 4
+print('1\n2\n3\n4\n5')
+
+# Exercise 5
+print("\"SDK\" stands for \"Software Development Kit\", whereas\n\"IDE\" stands for \"Integrated Development Environment\".")
+
+# Exercise 6
+print("python is an \"awesome\" language.")
+print("python\n\t2023")
+print('I\'m from Entri.\b')
+print("\65")
+print("\x65")
+print("Entri", "2023", sep="\n")
+print("Entri", "2023", sep="\b")
+print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+
+# Exercise 7
+num = 23
+textnum = "57"
+decimal = 98.3
+
+print("Type of num:", type(num))
+print("Type of textnum:", type(textnum))
+print("Type of decimal:", type(decimal))
+
+sum_of_vars = num + int(textnum) + decimal
+print("Sum of variables:", sum_of_vars)
+print("Type of sum:", type(sum_of_vars))
+
+# Exercise 8
+days_in_year = 365
+hours_in_day = 24
+minutes_in_hour = 60
+
+total_minutes = days_in_year * hours_in_day * minutes_in_hour
+print("The total number of minutes in a year is : " , total_minutes)
+
+# Exercise 9
+name = input("Please enter your name: ")
+print(f"Hi {name}, welcome to Python programming :)")
+
+# Exercise 10
+pounds = float(input("Please enter amount in pounds: £ "))
+dollars = pounds * 1.25
+print(f"£ {pounds} are $ {dollars}")
+
+
+
+
---
+++ PoundsToDollors.py
@@ -0,0 +1,3 @@
+pounds = float(input("Please enter amount in pounds: £ "))
+dollars = pounds * 1.25
+print(f"£ {pounds} are $ {dollars}")
