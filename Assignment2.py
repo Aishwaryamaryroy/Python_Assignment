3b26a0f8eb8de58a1f15572ada4dfa8f62511884
Aishwaryamaryroy <annammasroy@gmail.com>
2025-01-27 15:46:45 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,43 @@
+# List Q1
+random_numbers = [7, 14, 23, 35, 42]
+print("List of random numbers:", random_numbers)
+#Q2
+random_numbers.extend([56, 68, 79])
+print("Updated list:", random_numbers)
+#Q2
+for number in random_numbers:
+    print("Element:", number)
+#Dictionary Q1
+person = {"name": "John", "age": 25, "address": "New York"}
+print("Dictionary:", person)
+#Q2
+person["phone"] = "1234567890"
+print("Updated dictionary:", person)
+#Set Q1
+number_set = {1, 2, 3, 4, 5}
+print("Set:", number_set)
+#Q2
+number_set.add(6)
+print("Updated set:", number_set)
+#Q3
+number_set.remove(3)
+print("Set after removing 3:", number_set)
+#Tuple Q1
+number_tuple = (1, 2, 3, 4)
+print("Tuple:", number_tuple)
+#Q2
+print("Length of tuple:", len(number_tuple))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
